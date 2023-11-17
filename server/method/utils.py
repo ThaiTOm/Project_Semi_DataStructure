5978abc2c0829b7ca54963bbba17dd96f37ede50
Duy Thai <56117814+ThaiTOm@users.noreply.github.com>
2023-11-17 13:01:09 +0700

    a

--- server/method/utils.py
+++ server/method/utils.py
@@ -1,7 +1,7 @@
 import re
 import json
 
-file_ = open("storage.json", "rb")
+file_ = open("server/storage.json", "rb")
 config = json.load(file_)
 file_.close()
 
@@ -24,20 +24,38 @@ def write_json(path: str, data):
         print("There is an error in write_json function")
         print(e)
 
-def change_json(path: str, data, keys):
-    dataJson  = read_json(path)
-    dataJson[keys] = data
-    write_json(dataJson)
 
-def push_json(path:str, data, keys):
+def change_json(path: str, data, keys):
+    dataJson = read_json(path)
+    dataJson[keys] = data
+    write_json(path, dataJson)
+
+
+def push_json(path: str, data, keys):
     try:
-        data = read_json(path)
-        data["keys"].append(data)
-        write_json(path, data)
+        dataJson = read_json(path)
+        dataJson[keys].append(data)
+        write_json(path, dataJson)
     except Exception as e:
         print("There is an error in push Data Json ")
 
+
+def delete_json(path: str, data: list[int], keys):
+    try:
+        dataJson = read_json(path)
+
+        for temp in data:
+            dataJson[keys].pop(temp)
+        write_json(path, dataJson)
+    except Exception as e:
+        print(e)
+
+
 def read_json(path: str):
+    """
+    :param path: string
+    :return: data
+    """
     try:
         file_json = open(path, "rb")
         jsonObject_ = json.load(file_json)
@@ -49,6 +67,31 @@ def read_json(path: str):
 
 
 def validateFormData(formData, keys):
+    """
+    To validate if in form data contains all keys necessary
+    :param formData: list     :param keys:
+    :return: nothing if it's not wrong anything
+    """
     for key in keys:
         if key not in formData or formData[key] is None:
             assert f'Missing information {key} not in formData or {key} is None'
+
+
+def compareStrDate(fs: str, sc: str) -> int:
+    """
+    compare 2 input string of date and time
+    :param fs: first element to compare
+    :param sc: second element to compare
+    :return:
+    -1: fs < sc
+    0: equal
+    1: fs > sc
+    """
+    fs = int(fs)
+    sc = int(sc)
+    if fs == sc:
+        return 0
+    elif fs > sc:
+        return 1
+    else:
+        return -1
